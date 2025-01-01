f8eb4c1a4a4954f38308940452ec79d4111c7ad8
brad <bradleycarr@gmail.com>
2025-01-01 13:13:35 +0000

    add RicherConsoleTests, refactor where suitable

--- src/Wordle/RicherConsole.cs
+++ src/Wordle/RicherConsole.cs
@@ -2,6 +2,7 @@ using System.Text;
 
 namespace Wordle;
 
+/// <summary>Wraps an existing console and adds support for colored text and rainbow text.</summary>
 public class RicherConsole : IConsole
 {
     private static readonly Dictionary<string, ConsoleColor> ColorMap =
@@ -10,65 +11,99 @@ public class RicherConsole : IConsole
             .ToDictionary(z => z.First.ToLower(), z => z.Second, StringComparer.OrdinalIgnoreCase);
 
     private IConsole _inner;
+    private ConsoleColor _initialForegroundColor;
+    private ConsoleColor _initialBackgroundColor;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RicherConsole"/> class.
+    /// </summary>
+    /// <param name="inner">The inner console to wrap.</param>
     public RicherConsole(IConsole inner)
     {
         _inner = inner;
         Clear();
-        ResetColors();
+        _initialForegroundColor = ForegroundColor;
+        _initialBackgroundColor = BackgroundColor;
     }
 
+    /// <summary>
+    /// Gets or sets the background color of the inner console.
+    /// </summary>
     public ConsoleColor BackgroundColor
     {
         get => _inner.BackgroundColor;
         set => _inner.BackgroundColor = value;
     }
 
+    /// <summary>
+    /// Gets or sets the foreground color of the inner console.
+    /// </summary>
     public ConsoleColor ForegroundColor
     {
         get => _inner.ForegroundColor;
         set => _inner.ForegroundColor = value;
     }
 
+    /// <summary>
+    /// Clears the inner console.
+    /// </summary>
     public void Clear() => _inner.Clear();
 
+    /// <summary>
+    /// Reads the next line of characters from the inner console.
+    /// </summary>
+    /// <returns>
+    /// The next line of characters from the inner console, or null if the end of the input stream has been reached.
+    /// </returns>
     public string? ReadLine()
     {
+        var restoreForegroundColor = ForegroundColor;
         ForegroundColor = ConsoleColor.Yellow;
         var input = _inner.ReadLine();
-        ResetColors();
+        if (restoreForegroundColor != ConsoleColor.Yellow)
+        {
+            ForegroundColor = restoreForegroundColor;
+        }
         return input;
     }
 
-    public void Write(string text)
+    /// <summary>
+    /// Writes markup text to the console, with support for colored text and rainbow text.
+    /// </summary>
+    /// <param name="markup">
+    /// The text to write. This text can contain color markers in the form of $colorName(...).
+    /// </param>
+    /// <remarks>
+    /// The color markers are:
+    /// - $colorName(...) - sets the color of the text that follows. The colorName is case-insensitive.
+    /// - $rainbow(...) - sets the color of each character in the text that follows to a different color.
+    /// </remarks>
+    /// <example>
+    ///   <code>
+    ///     console.Write("$green(Hello), $red(world)! $rainbow(Rainbow text!)");
+    ///   </code>
+    /// </example>
+    public void Write(string markup)
     {
-        ResetColors();
         var rainbow = false;
         var buf = new StringBuilder();
-        ConsoleColor nextColor;
-        foreach (var c in text)
+        var colorStack = new Stack<ConsoleColor>();
+        foreach (var c in markup)
         {
-            if (c == '$')
+            if (c == '(' && TryExtractColorChangeMarkup(buf, out var colorLiteral))
             {
-                _inner.Write(buf.ToString());
-                buf.Clear();
-            }
-            else if (c == '(')
-            {
-                var colorLiteral = buf.ToString().ToLower();
-                rainbow = string.Equals(
-                    colorLiteral,
-                    "rainbow",
-                    StringComparison.OrdinalIgnoreCase
-                );
-                if (!rainbow)
+                FlushBuffer(buf);
+
+                colorStack.Push(ForegroundColor);
+
+                if (!(rainbow = colorLiteral == "rainbow"))
                 {
-                    ForegroundColor = nextColor = ColorMap[buf.ToString()];
+                    ForegroundColor = ColorMap[colorLiteral!];
                 }
 
                 buf.Clear();
             }
-            else if (c == ')')
+            else if (c == ')' && colorStack.Count > 0)
             {
                 if (rainbow)
                 {
@@ -76,54 +111,101 @@ public class RicherConsole : IConsole
                 }
                 else
                 {
-                    _inner.Write(buf.ToString());
+                    FlushBuffer(buf);
                 }
 
                 buf.Clear();
-                ResetColors();
+                ForegroundColor = colorStack.Pop();
             }
             else
             {
                 buf.Append(c);
             }
         }
-        _inner.Write(buf.ToString());
+
+        if (colorStack.Count > 0)
+        {
+            throw new InvalidOperationException("$<color>(...) expression not terminated.");
+        }
+
+        FlushBuffer(buf);
     }
 
+    private void FlushBuffer(StringBuilder buf)
+    {
+        if (buf.Length > 0)
+        {
+            _inner.Write(buf.ToString());
+            buf.Clear();
+        }
+    }
+
+    private static bool TryExtractColorChangeMarkup(StringBuilder buf, out string? colorLiteral)
+    {
+        var i = buf.Length - 1;
+        while (i >= 0 && buf[i] != '$')
+        {
+            i--;
+        }
+
+        if (i < 0)
+        {
+            colorLiteral = null;
+            return false;
+        }
+
+        colorLiteral = buf.ToString(i + 1, buf.Length - i - 1).ToLower();
+        buf.Remove(i, buf.Length - i);
+        return true;
+    }
+
+    /// <summary>
+    /// Writes a character to the inner console.
+    /// </summary>
     public void Write(char c) => _inner.Write(c);
 
+    /// <summary>
+    /// Writes a line terminator to the inner console.
+    /// </summary>
     public void WriteLine() => _inner.WriteLine();
 
-    public void WriteLine(string text)
+    /// <summary>
+    /// Writes markup text followed by a line terminator to the inner console.
+    /// </summary>
+    /// <param name="markup">
+    /// The text to write. This text can contain color markers in the form of $colorName(...).
+    /// </param>
+    /// <remarks>
+    /// The color markers are:
+    /// - $colorName(...) - sets the color of the text that follows. The colorName is case-insensitive.
+    /// - $rainbow(...) - sets the color of each character in the text that follows to a different color.
+    /// </remarks>
+    /// <example>
+    ///   <code>
+    ///     console.Write("$green(Hello), $red(world)! $rainbow(Rainbow text!)");
+    ///   </code>
+    /// </example>
+    public void WriteLine(string markup)
     {
-        Write(text);
+        Write(markup);
         WriteLine();
     }
 
     private void WriteRainbow(StringBuilder buf)
     {
-        _inner.ForegroundColor = BackgroundColor;
-        RotateForegroundColor();
+        var nextForegroundColor = RotateForegroundColor(BackgroundColor);
 
         foreach (var k in buf.ToString())
         {
+            ForegroundColor = nextForegroundColor;
             Write(k);
-            RotateForegroundColor();
-
-            if (ForegroundColor == BackgroundColor)
-            {
-                RotateForegroundColor();
-            }
+            nextForegroundColor = RotateForegroundColor(nextForegroundColor);
         }
     }
 
-    private void RotateForegroundColor() =>
-        ForegroundColor =
-            ForegroundColor == ConsoleColor.White ? ConsoleColor.Black : ForegroundColor + 1;
-
-    public void ResetColors()
+    private ConsoleColor RotateForegroundColor(ConsoleColor foregroundColor)
     {
-        BackgroundColor = ConsoleColor.Black;
-        ForegroundColor = ConsoleColor.Gray;
+        var next = foregroundColor == ConsoleColor.White ? ConsoleColor.Black : foregroundColor + 1;
+        return next == BackgroundColor ? RotateForegroundColor(next) : next;
     }
 }
---
+++ tests/Wordle.UnitTests/RicherConsoleTests.cs
@@ -0,0 +1,306 @@
+using FluentAssertions;
+using Moq;
+
+namespace Wordle.UnitTests
+{
+    public class RicherConsoleTests
+    {
+        private const ConsoleColor DefaultBackgroundColor = ConsoleColor.Black;
+        private const ConsoleColor DefaultForegroundColor = ConsoleColor.Gray;
+
+        [Fact]
+        public void GetBackgroundColor_SourcedFromInnerConsole()
+        {
+            var inner = new Mock<IConsole>(MockBehavior.Strict);
+            var sut = CreateAndVerifySUT(inner);
+
+            inner.SetupGet(mock => mock.BackgroundColor).Returns(ConsoleColor.Yellow).Verifiable();
+
+            sut.BackgroundColor.Should().Be(ConsoleColor.Yellow);
+
+            inner.VerifyGet(mock => mock.BackgroundColor, Times.Once());
+            inner.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public void GetForegroundColor_SourcedFromInnerConsole()
+        {
+            var inner = new Mock<IConsole>(MockBehavior.Strict);
+            var sut = CreateAndVerifySUT(inner);
+
+            inner.SetupGet(mock => mock.ForegroundColor).Returns(ConsoleColor.Yellow).Verifiable();
+
+            sut.ForegroundColor.Should().Be(ConsoleColor.Yellow);
+
+            inner.VerifyGet(mock => mock.ForegroundColor, Times.Once());
+            inner.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public void SetBackgroundColor_PropagatesToInnerConsole()
+        {
+            var inner = new Mock<IConsole>(MockBehavior.Strict);
+            var sut = CreateAndVerifySUT(inner);
+
+            inner.SetupSet(mock => mock.BackgroundColor = ConsoleColor.Yellow).Verifiable();
+
+            sut.BackgroundColor = ConsoleColor.Yellow;
+
+            inner.VerifySet(mock => mock.BackgroundColor = ConsoleColor.Yellow, Times.Once());
+            inner.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public void SetForegroundColor_PropagatesToInnerConsole()
+        {
+            var inner = new Mock<IConsole>(MockBehavior.Strict);
+            var sut = CreateAndVerifySUT(inner);
+
+            inner.SetupSet(mock => mock.ForegroundColor = ConsoleColor.Yellow).Verifiable();
+            sut.ForegroundColor = ConsoleColor.Yellow;
+
+            inner.VerifySet(mock => mock.ForegroundColor = ConsoleColor.Yellow, Times.Once());
+            inner.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public void ReadLine_ReturnsInputFromInnerConsole()
+        {
+            var inner = new Mock<IConsole>(MockBehavior.Strict);
+            var sut = CreateAndVerifySUT(inner);
+
+            inner.SetupSet(mock => mock.ForegroundColor = ConsoleColor.Yellow).Verifiable();
+            inner
+                .SetupGet(mock => mock.ForegroundColor)
+                .Returns(DefaultForegroundColor)
+                .Verifiable();
+            inner.Setup(mock => mock.ReadLine()).Returns("Hello world!").Verifiable();
+            inner.SetupSet(mock => mock.ForegroundColor = DefaultForegroundColor).Verifiable();
+
+            var result = sut.ReadLine();
+
+            result.Should().Be("Hello world!");
+            inner.Verify(mock => mock.ReadLine(), Times.Once());
+            inner.VerifySet(mock => mock.ForegroundColor = ConsoleColor.Yellow, Times.Once());
+            inner.VerifySet(mock => mock.ForegroundColor = DefaultForegroundColor, Times.Once());
+            inner.VerifyGet(mock => mock.ForegroundColor, Times.Once());
+            inner.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public void Write_PlainText()
+        {
+            var inner = new Mock<IConsole>(MockBehavior.Strict);
+            var sut = CreateAndVerifySUT(inner);
+
+            inner.Setup(mock => mock.Write("Hello world!")).Verifiable();
+
+            sut.Write("Hello world!");
+
+            inner.Verify(mock => mock.Write("Hello world!"), Times.Once());
+            inner.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public void WriteLine_PlainText()
+        {
+            var inner = new Mock<IConsole>(MockBehavior.Strict);
+            var sut = CreateAndVerifySUT(inner);
+
+            inner.Setup(mock => mock.Write("Hello world!")).Verifiable();
+            inner.Setup(mock => mock.WriteLine()).Verifiable();
+
+            sut.WriteLine("Hello world!");
+
+            inner.Verify(mock => mock.Write("Hello world!"), Times.Once());
+            inner.Verify(mock => mock.WriteLine(), Times.Once());
+            inner.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData("(not markup)")]
+        [InlineData("(not markup) Hello world! (not markup)")]
+        public void Write_PlainTextBracesNotConsideredMarkup(string plainText)
+        {
+            var inner = new Mock<IConsole>(MockBehavior.Strict);
+            var sut = CreateAndVerifySUT(inner);
+
+            inner.Setup(mock => mock.Write(plainText)).Verifiable();
+
+            sut.Write(plainText);
+
+            inner.Verify(mock => mock.Write(plainText), Times.Once());
+            inner.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public void Write_Markup()
+        {
+            var inner = new Mock<IConsole>(MockBehavior.Strict);
+            var sut = CreateAndVerifySUT(inner);
+
+            inner
+                .SetupGet(mock => mock.ForegroundColor)
+                .Returns(DefaultForegroundColor)
+                .Verifiable();
+            inner.SetupSet(mock => mock.ForegroundColor = ConsoleColor.Green).Verifiable();
+            inner.SetupSet(mock => mock.ForegroundColor = DefaultForegroundColor).Verifiable();
+            inner.SetupSet(mock => mock.ForegroundColor = ConsoleColor.Red).Verifiable();
+            inner.Setup(mock => mock.Write("Hello")).Verifiable();
+            inner.Setup(mock => mock.Write(" ")).Verifiable();
+            inner.Setup(mock => mock.Write("world!")).Verifiable();
+
+            sut.Write("$green(Hello) $red(world!)");
+
+            inner.Verify(mock => mock.Write("Hello"), Times.Once());
+            inner.Verify(mock => mock.Write(" "), Times.Once());
+            inner.Verify(mock => mock.Write("world!"), Times.Once());
+            inner.VerifySet(mock => mock.ForegroundColor = ConsoleColor.Green, Times.Once());
+            inner.VerifySet(mock => mock.ForegroundColor = ConsoleColor.Red, Times.Once());
+            inner.VerifySet(
+                mock => mock.ForegroundColor = DefaultForegroundColor,
+                Times.Exactly(2)
+            );
+            inner.VerifyGet(mock => mock.ForegroundColor, Times.Exactly(2));
+            inner.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public void Write_MarkupNotTerminated()
+        {
+            var inner = new Mock<IConsole>(MockBehavior.Strict);
+            var sut = CreateAndVerifySUT(inner);
+
+            inner
+                .SetupGet(mock => mock.ForegroundColor)
+                .Returns(DefaultForegroundColor)
+                .Verifiable();
+            inner.SetupSet(mock => mock.ForegroundColor = ConsoleColor.Green).Verifiable();
+
+            sut.Invoking(x => x.Write("$green(Not terminated")) // missing closing parenthesis
+                .Should()
+                .Throw<InvalidOperationException>()
+                .WithMessage("$<color>(...) expression not terminated.");
+
+            inner.VerifyGet(mock => mock.ForegroundColor, Times.Once());
+            inner.VerifySet(mock => mock.ForegroundColor = ConsoleColor.Green, Times.Once());
+            inner.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public void Write_MarkupRainbowCyclesColors()
+        {
+            var inner = new Mock<IConsole>(MockBehavior.Strict);
+            var sut = CreateAndVerifySUT(inner);
+
+            inner
+                .SetupGet(mock => mock.ForegroundColor)
+                .Returns(DefaultForegroundColor)
+                .Verifiable();
+
+            inner.Setup(mock => mock.Write("plain text ")).Verifiable();
+            inner.SetupSet(mock => mock.ForegroundColor = ConsoleColor.DarkBlue).Verifiable();
+            inner.Setup(mock => mock.Write('R')).Verifiable();
+            inner.SetupSet(mock => mock.ForegroundColor = ConsoleColor.DarkGreen).Verifiable();
+            inner.Setup(mock => mock.Write('a')).Verifiable();
+            inner.SetupSet(mock => mock.ForegroundColor = ConsoleColor.DarkCyan).Verifiable();
+            inner.Setup(mock => mock.Write('i')).Verifiable();
+            inner.SetupSet(mock => mock.ForegroundColor = ConsoleColor.DarkRed).Verifiable();
+            inner.Setup(mock => mock.Write('n')).Verifiable();
+            inner.SetupSet(mock => mock.ForegroundColor = ConsoleColor.DarkMagenta).Verifiable();
+            inner.Setup(mock => mock.Write('b')).Verifiable();
+            inner.SetupSet(mock => mock.ForegroundColor = ConsoleColor.DarkYellow).Verifiable();
+            inner.Setup(mock => mock.Write('o')).Verifiable();
+            inner.SetupSet(mock => mock.ForegroundColor = ConsoleColor.Gray).Verifiable();
+            inner.Setup(mock => mock.Write('w')).Verifiable();
+            inner.SetupSet(mock => mock.ForegroundColor = ConsoleColor.DarkGray).Verifiable();
+            inner.Setup(mock => mock.Write(' ')).Verifiable();
+            inner.SetupSet(mock => mock.ForegroundColor = ConsoleColor.Blue).Verifiable();
+            inner.Setup(mock => mock.Write('T')).Verifiable();
+            inner.SetupSet(mock => mock.ForegroundColor = ConsoleColor.Green).Verifiable();
+            inner.Setup(mock => mock.Write('e')).Verifiable();
+            inner.SetupSet(mock => mock.ForegroundColor = ConsoleColor.Cyan).Verifiable();
+            inner.Setup(mock => mock.Write('x')).Verifiable();
+            inner.SetupSet(mock => mock.ForegroundColor = ConsoleColor.Red).Verifiable();
+            inner.Setup(mock => mock.Write('t')).Verifiable();
+            inner.SetupSet(mock => mock.ForegroundColor = ConsoleColor.Magenta).Verifiable();
+            inner.Setup(mock => mock.Write('!')).Verifiable();
+            inner.SetupSet(mock => mock.ForegroundColor = DefaultForegroundColor).Verifiable();
+            inner.Setup(mock => mock.Write(" more plain text")).Verifiable();
+
+            sut.Write("plain text $rainbow(Rainbow Text!) more plain text");
+
+            inner.Verify(mock => mock.Write("plain text "), Times.Once());
+            inner.Verify(mock => mock.Write('R'));
+            inner.Verify(mock => mock.Write('a'));
+            inner.Verify(mock => mock.Write('i'));
+            inner.Verify(mock => mock.Write('n'));
+            inner.Verify(mock => mock.Write('b'));
+            inner.Verify(mock => mock.Write('o'));
+            inner.Verify(mock => mock.Write('w'));
+            inner.Verify(mock => mock.Write(' '));
+            inner.Verify(mock => mock.Write('T'));
+            inner.Verify(mock => mock.Write('e'));
+            inner.Verify(mock => mock.Write('x'));
+            inner.Verify(mock => mock.Write('t'));
+            inner.Verify(mock => mock.Write('!'));
+            inner.Verify(mock => mock.Write(" more plain text"));
+
+            inner.VerifySet(mock => mock.ForegroundColor = ConsoleColor.DarkBlue, Times.Once());
+            inner.VerifySet(mock => mock.ForegroundColor = ConsoleColor.DarkGreen, Times.Once());
+            inner.VerifySet(mock => mock.ForegroundColor = ConsoleColor.DarkCyan, Times.Once());
+            inner.VerifySet(mock => mock.ForegroundColor = ConsoleColor.DarkRed, Times.Once());
+            inner.VerifySet(mock => mock.ForegroundColor = ConsoleColor.DarkMagenta, Times.Once());
+            inner.VerifySet(mock => mock.ForegroundColor = ConsoleColor.DarkYellow, Times.Once());
+            // inner.VerifySet(mock => mock.ForegroundColor = ConsoleColor.Gray, Times.Once()); conflicts with default foreground color
+            inner.VerifySet(mock => mock.ForegroundColor = ConsoleColor.DarkGray, Times.Once());
+            inner.VerifySet(mock => mock.ForegroundColor = ConsoleColor.Blue, Times.Once());
+            inner.VerifySet(mock => mock.ForegroundColor = ConsoleColor.Green, Times.Once());
+            inner.VerifySet(mock => mock.ForegroundColor = ConsoleColor.Cyan, Times.Once());
+            inner.VerifySet(mock => mock.ForegroundColor = ConsoleColor.Red, Times.Once());
+            inner.VerifySet(mock => mock.ForegroundColor = ConsoleColor.Magenta, Times.Once());
+            inner.VerifySet(
+                mock => mock.ForegroundColor = DefaultForegroundColor,
+                Times.Exactly(2)
+            );
+            inner.VerifyGet(mock => mock.ForegroundColor, Times.Once());
+            inner.VerifyGet(mock => mock.BackgroundColor, Times.Exactly(15));
+            inner.VerifyNoOtherCalls();
+        }
+
+        private static RicherConsole CreateAndVerifySUT(Mock<IConsole> inner)
+        {
+            inner
+                .SetupGet(mock => mock.ForegroundColor)
+                .Returns(DefaultForegroundColor)
+                .Verifiable();
+            inner
+                .SetupGet(mock => mock.BackgroundColor)
+                .Returns(DefaultBackgroundColor)
+                .Verifiable();
+            inner.Setup(mock => mock.Clear()).Verifiable();
+
+            var sut = new RicherConsole(inner.Object);
+
+            inner.VerifyGet(
+                mock => mock.ForegroundColor,
+                Times.Once(),
+                "should capture the initial foreground color on initialization"
+            );
+            inner.VerifyGet(
+                mock => mock.BackgroundColor,
+                Times.Once(),
+                "should capture the initial background color on initialization"
+            );
+            inner.Verify(
+                mock => mock.Clear(),
+                Times.Once(),
+                "should clear the console on initialization"
+            );
+            inner.VerifyNoOtherCalls();
+            inner.Invocations.Clear();
+
+            return sut;
+        }
+    }
+}
